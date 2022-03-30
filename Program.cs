8a204eb373d3865d0342ba346337d4469909487b
kbezrukova <95983771+kbezrukova@users.noreply.github.com>
2022-03-31 02:37:51 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,43 +1,54 @@
-﻿//2 задание
+﻿//1 задание
 using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Барс2
+using System.IO;
+using System.Management.Automation.Language;
+//я не знаю правильно ли, но я вложила в это душу..
+namespace Барс1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<Entity> list = new List<Entity>
-                {
-                    new Entity{Id=1, ParentId=0, Name="Root entity"},
-                    new Entity { Id = 2, ParentId = 1, Name = "Child of 1 entity"},
-                    new Entity { Id = 3, ParentId = 1, Name = "Child of 1 entity"},
-                    new Entity { Id = 4, ParentId = 2, Name = "Child of 2 entity"},
-                    new Entity { Id = 5, ParentId = 4, Name = "Child of 4 entity"}
-                };
-            Method(list);     
-        }
-        public static void Method(List<Entity> list)
-            {               
-                Dictionary<int, Entity> dictionary = list.ToDictionary(p =>p.ParentId);
-                foreach (KeyValuePair<int, Entity> kvp in dictionary)
-                {
-                    Console.WriteLine(
-                        "Key {0}: {1}, {2} pounds",
-                        kvp.Key,
-                        kvp.Value.Id);
-                }
-            }
-    }
-    class Entity
-    {
-        public int Id { get; set; }
-        public int ParentId { get; set; }
-        public string Name { get; set; }
-        
-    }
-    
+            string textFile= @"C:\Users\1\source\repos\Барс1\Барс1\bars1.txt";
+            LocalFileLogger<string> test1 = new LocalFileLogger<string>(textFile);
+            test1.LogInfo("Hello");
+            test1.LogWarning("Stop");
+            Exception ex = new Exception("Unknown exception");
+            test1.LogError("Ex", ex);
 
+            LocalFileLogger<byte> test2 = new LocalFileLogger<byte>(textFile);
+            test2.LogInfo("Hello, hello!!!");
+            test2.LogWarning("Stop!!!");
+            Exception ex2 = new Exception("Unknown exception...");
+            test2.LogError("Ex", ex);
+        }
+    }
+
+    public interface ILogger
+    {
+        public void LogInfo(string message);
+        public void LogWarning(string message);
+        public void LogError(string message, Exception ex);
+    }
+    public class LocalFileLogger<T> : ILogger
+    {
+        private string path;
+        public LocalFileLogger(string pathToFile)
+        {
+            path = Path.GetFullPath(pathToFile);
+        }
+        string GenericTypeName=typeof(T).Name;
+        public void LogInfo(string message)
+        {
+            File.AppendAllText(path, $"[Info]: [{GenericTypeName}] : {message}" +Environment.NewLine);
+        }
+        public void LogWarning(string message)
+        {
+            File.AppendAllText(path, $"[Warning] : [{GenericTypeName}] : {message}" + Environment.NewLine);
+        }
+        public void LogError(string message, Exception ex)
+        {
+            File.AppendAllText(path, $"[Error] : [{GenericTypeName}] : {message}. {ex.Message}" + Environment.NewLine);
+        }
+    }
 }
